ccff00e283d2a7350faf9c614435bd362b0a4a65
CodePlexbofcarbon1 <bofcarbon1@gmail.com>
2020-10-21 01:19:23 -0600

    week 6 changes

--- src/app/player/player-list/player-list.component.ts
+++ src/app/player/player-list/player-list.component.ts
@@ -19,26 +19,26 @@ export class PlayerListComponent implements OnInit {
       {id: "0", name: "Phillip Rivers", position: "QB", age: "38", 
       note: "2018 Pro Bowl selection played previously with the San Diego Chargers. New adventures with the Colts.",
       keystats: [
-        {stat: "cmp", value: "70.8%"}, 
-        {stat: "passYds", value: "1,227"}, 
-        {stat: "ints", value: "5"},
-        {stat: "tds", value: "4"},
-        {stat: "rtng", value: "88.7"},
-        {stat: "QBR", value: "59.4"}
+        {stat: "cmp", value: "69.7%"}, 
+        {stat: "passYds", value: "1,598"}, 
+        {stat: "ints", value: "6"},
+        {stat: "tds", value: "7"},
+        {stat: "rtng", value: "93"},
+        {stat: "QBR", value: "64.7"}
         ], },
       {id: "1", name: "Xavier Rhodes", position: "CB", age: "30",
       note: "The once Pro Bowl player had 2 big interceptions against the Jets in game 3. ",
       keystats: [
         {stat: "int", value: "2"},
-        {stat: "solo", value: "7"},
+        {stat: "solo", value: "10"},
         {stat: "pd", value: "5"}        
         ], },
       {id: "2", name: "Deforest Buckner", position: "DB", age: "27",
       note: "Former All Pro with San Fransisco 49ers. Acquired in deal for 2020 1st round draft pick", 
       keystats: [
-        {stat: "takles", value: "23"},
-        {stat: "sacks", value: "1.5"},
-        {stat: "assists", value: "10"},
+        {stat: "takles", value: "28"},
+        {stat: "sacks", value: "2.5"},
+        {stat: "assists", value: "17"},
         {stat: "ints", value: "0"}
         ], }
     ];
@@ -51,19 +51,19 @@ export class PlayerListComponent implements OnInit {
         {stat: "sacks", value: "0"},
         {stat: "ints", value: "0"}
         ], },
-      {id: "1", name: "Moe Allie-Cox", position: "TE", age: "27",
-        note: "Another TD in game 4. Keeps making a strong argument for TE1.",
+      {id: "1", name: "TY Hilton", position: "WR", age: "30",
+        note: "Four straight Pro Bowls from 2014 to 2018.",
         keystats: [
-          {stat: "rec", value: "12"}, 
-          {stat: "recYds", value: "194"},
-          {stat: "avg", value: "17.6"},
-          {stat: "lng", value: "45"},
-          {stat: "td", value: "2"}
+          {stat: "rec", value: "20"}, 
+          {stat: "recYds", value: "242"},
+          {stat: "avg", value: "12.1"},
+          {stat: "lng", value: "28"},
+          {stat: "td", value: "0"}
           ], },
       {id: "2", name: "Anthony Walker", position: "LB", age: "25",
       note: "Always dependable on defense", 
       keystats: [
-        {stat: "tackles", value: "32"},
+        {stat: "tackles", value: "39"},
         {stat: "assists", value: "7"},
         {stat: "sacks", value: "0"},
         {stat: "ints", value: "1"}
@@ -73,28 +73,28 @@ export class PlayerListComponent implements OnInit {
     {id: "0", name: "Rodrigo Blakenship", position: "K", age: "23",
     note: "At Georgia undrafted free agent nicknamed Hot Rod", 
     keystats: [
-      {stat: "fg%", value: "88.2"},
+      {stat: "fg%", value: "88.9"},
       {stat: "xp%", value: "100.0"}, 
       {stat: "lng", value: "44"},
-      {stat: "pts", value: "56"}      
+      {stat: "pts", value: "63"}      
       ], },
     {id: "1", name: "Jonathon Taylor", position: "RB", age: "21",
     note: "At Wisconson No. 6 all time rusher in the NCAA and first player to rush for more than 6,000 yards in a three-year span.", 
     keystats: [
-      {stat: "rushAtt", value: "77"},
-      {stat: "rushYds", value: "307"},
-      {stat: "avg", value: "4.0"},
-      {stat: "lng", value: "16"},
+      {stat: "rushAtt", value: "89"},
+      {stat: "rushYds", value: "367"},
+      {stat: "avg", value: "4.1"},
+      {stat: "lng", value: "21"},
       {stat: "td", value: "3"}
       ], },
     {id: "2", name: "Julian Blackmon", position: "S", age: "22",
     note: "Out of Utah, 6.0 187-pound, has been thrown into play earlier than expected due to the loss of Malik Hooker.", 
     keystats: [
-      {stat: "tackles", value: "8"},
+      {stat: "tackles", value: "10"},
       {stat: "FF", value: "0"},
-      {stat: "solo", value: "7"}, 
-      {stat: "int", value: "1"},
-      {stat: "pd", value: "5"}
+      {stat: "solo", value: "9"}, 
+      {stat: "int", value: "2"},
+      {stat: "pd", value: "6"}
       ], }
   ]    
   displayedColumns: string[] = ['name', 'position', 'info'];
