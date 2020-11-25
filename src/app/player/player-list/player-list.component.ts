d240263583b34af012a91e78390f8c2803381df1
CodePlexbofcarbon1 <bofcarbon1@gmail.com>
2020-11-24 21:03:16 -0700

    week 11 updates'

--- src/app/player/player-list/player-list.component.ts
+++ src/app/player/player-list/player-list.component.ts
@@ -19,37 +19,37 @@ export class PlayerListComponent implements OnInit {
       {id: "0", name: "Phillip Rivers", position: "QB", age: "38", 
       note: "2018 Pro Bowl selection played previously with the San Diego Chargers. New adventures with the Colts.",
       keystats: [
-        {stat: "cmp", value: "68.7%"}, 
-        {stat: "passYds", value: "2,395"}, 
-        {stat: "ints", value: "7"},
-        {stat: "tds", value: "11"},
-        {stat: "rtng", value: "94.7"},
-        {stat: "QBR", value: "64.1"}
+        {stat: "cmp", value: "68.5%"}, 
+        {stat: "passYds", value: "2,683"}, 
+        {stat: "ints", value: "8"},
+        {stat: "tds", value: "14"},
+        {stat: "rtng", value: "95"},
+        {stat: "QBR", value: "64.7"}
         ], },
       {id: "1", name: "Grover Stewart", position: "DT", age: "30",
       note: "PFF 90s Club with a 90.6 rating in week 9. ",
       keystats: [
         {stat: "int", value: "0"},
-        {stat: "tackles", value: "34"}, 
+        {stat: "tackles", value: "36"}, 
         {stat: "solo", value: "24"}                
         ], },
       {id: "2", name: "Deforest Buckner", position: "DB", age: "27",
       note: "Former All Pro with San Fransisco 49ers. Acquired in deal for 2020 1st round draft pick", 
       keystats: [
-        {stat: "takles", value: "39"},
+        {stat: "takles", value: "41"},
         {stat: "sacks", value: "2.5"},
-        {stat: "solo", value: "22"},
-        {stat: "ints", value: "0"}
+        {stat: "solo", value: "24"},
+        {stat: "fr", value: "1"}
         ], }
     ];
   tops = [
       {id: "0", name: "Darius Leonard", position: "DB", age: "25",
       note: "Former Defensive Rookie of the Year and All Pro. Looking All Pro again in 2020.",
       keystats: [
-        {stat: "takles", value: "60"},
-        {stat: "solo", value: "40" },
+        {stat: "takles", value: "66"},
+        {stat: "solo", value: "44" },
         {stat: "sacks", value: "1"},
-        {stat: "ints", value: "0"}
+        {stat: "fr", value: "2"}
         ], },
       {id: "1", name: "Denico Autry", position: "DE", age: "30",
         note: "7th season in the NFL.",
@@ -61,8 +61,8 @@ export class PlayerListComponent implements OnInit {
       {id: "2", name: "Bobby Okereke", position: "LB", age: "24",
       note: "Establishing himself as effective force on defense.", 
       keystats: [
-        {stat: "tackles", value: "53"},
-        {stat: "solo", value: "41"},
+        {stat: "tackles", value: "54"},
+        {stat: "solo", value: "42"},
         {stat: "sacks", value: "0"},
         {stat: "ints", value: "1"}
         ], }        
@@ -71,26 +71,26 @@ export class PlayerListComponent implements OnInit {
     {id: "0", name: "Rodrigo Blakenship", position: "K", age: "23",
     note: "At Georgia undrafted free agent nicknamed Hot Rod", 
     keystats: [
-      {stat: "fg%", value: "90.5"},
-      {stat: "xp%", value: "92"}, 
+      {stat: "fg%", value: "88.5"},
+      {stat: "xp%", value: "92.6"}, 
       {stat: "lng", value: "44"},
-      {stat: "pts", value: "80"}      
+      {stat: "pts", value: "94"}      
       ], },
     {id: "1", name: "Michael Pittman", position: "WR", age: "21",
     note: "Out of USC returned from the IR got 101 receving yards against the Titans in game 9.", 
     keystats: [
-      {stat: "rec", value: "11"},
-      {stat: "recYds", value: "236"},
-      {stat: "avg", value: "11.2"},
-      {stat: "lng", value: "40"},
-      {stat: "td", value: "0"}
+      {stat: "rec", value: "24"},
+      {stat: "recYds", value: "302"},
+      {stat: "avg", value: "12.6"},
+      {stat: "lng", value: "45"},
+      {stat: "td", value: "1"}
       ], },
     {id: "2", name: "Julian Blackmon", position: "S", age: "22",
     note: "Out of Utah, 6.0 187-pound, has been thrown into play earlier than expected due to the loss of Malik Hooker.", 
     keystats: [
-      {stat: "tackles", value: "21"},
-      {stat: "FF", value: "0"},
-      {stat: "solo", value: "18"}, 
+      {stat: "tackles", value: "26"},
+      {stat: "FF", value: "1"},
+      {stat: "solo", value: "23"}, 
       {stat: "int", value: "2"},
       {stat: "pd", value: "6"}
       ], }
