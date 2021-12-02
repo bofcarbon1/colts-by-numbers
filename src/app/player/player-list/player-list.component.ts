6eb8288ef6c3083ad662b90e9783dc9e101c4983
bofcarbon1 <bofcarbon1@gmail.com>
2021-12-01 19:33:44 -0700

    2021 game 12 stats

--- src/app/player/player-list/player-list.component.ts
+++ src/app/player/player-list/player-list.component.ts
@@ -19,23 +19,23 @@ export class PlayerListComponent implements OnInit {
       {id: "0", name: "Carson Wentz", position: "QB", age: "28",
       note: "Acquired in a trade with the Philidelphia Eagles. New adventures with the Colts.",
       keystats: [
-        {stat: "cmp", value: "63.0%"},
-        {stat: "passYds", value: "2484"},
-        {stat: "ints", value: "3"},
-        {stat: "tds", value: "18"},
-        {stat: "rtng", value: "97.2"}
+        {stat: "cmp", value: "62.8%"},
+        {stat: "passYds", value: "2790"},
+        {stat: "ints", value: "5"},
+        {stat: "tds", value: "21"},
+        {stat: "rtng", value: "96.0"}
         ], },
       {id: "1", name: "DeForest Buckner", position: "DT", age: "26",
       note: "Leading the Colts in sacks in 2021.",
       keystats: [
-        {stat: "sacks", value: "4.5"},
-        {stat: "solo", value: "24"},
+        {stat: "sacks", value: "5.5"},
+        {stat: "solo", value: "27"},
         {stat: "ff", value: "0"}
         ], },
       {id: "2", name: "Bobby Okereke", position: "LB", age: "25",
       note: "Getting his share of tackles in 2021",
       keystats: [
-        {stat: "solo", value: "62"},
+        {stat: "solo", value: "68"},
         {stat: "sack", value: "1"}
         ], }
     ];
@@ -43,44 +43,44 @@ export class PlayerListComponent implements OnInit {
       {id: "0", name: "Jonathon Taylor", position: "RB", age: "25",
       note: "A stud in 2021. Rushing, receiving just about everywhere.",
       keystats: [
-        {stat: "run atts", value: "193"},
-        {stat: "run yds", value: "1122" },
+        {stat: "run atts", value: "209"},
+        {stat: "run yds", value: "1205" },
         {stat: "avg yds", value: "5.8" },
-        {stat: "tds", value: "13" },
-        {stat: "rec atts", value: "38"},
-        {stat: "rec yds", value: "322"},
-        {stat: "y/r", value: "10.1"}
+        {stat: "tds", value: "14" },
+        {stat: "rec atts", value: "36"},
+        {stat: "rec yds", value: "336"},
+        {stat: "y/r", value: "7.8"}
         ], },
       {id: "1", name: "Kenny Moore", position: "CB", age: "26",
         note: "Leading the Colts in solo tackles.",
         keystats: [
           {stat: "sacks", value: "1"},
-          {stat: "solo", value: "58" },
+          {stat: "solo", value: "59" },
           {stat: "int", value: "3" }
           ], },
       {id: "2", name: "Michael Pittman", position: "WR", age: "24",
       note: "Leading the WR group in yds.",
       keystats: [
-        {stat: "rec", value: "57"},
-        {stat: "yds", value: "752"},
+        {stat: "rec", value: "61"},
+        {stat: "yds", value: "805"},
         {stat: "tds", value: "5"},
-        {stat: "y/r", value: "13"}
+        {stat: "y/r", value: "8.8"}
         ], }
     ];
   rookies = [
     {id: "0", name: "Kylen Granson", position: "TE", age: "21",
     note: "Out of Charleston.",
     keystats: [
-      {stat: "rec", value: "7"},
-      {stat: "recYds", value: "77"},
-      {stat: "avg", value: "10"},
+      {stat: "rec", value: "9"},
+      {stat: "recYds", value: "94"},
+      {stat: "avg", value: "7.8"},
       {stat: "td", value: "0"}
       ], },
     {id: "1", name: "Kwitty Paye", position: "DE", age: "22",
     note: "Out of Michigan, 6.2 261-pound, figures to make a big impact on the pass rush game.",
     keystats: [
-      {stat: "solo", value: "11"},
-      {stat: "sacks", value: "2"},
+      {stat: "solo", value: "13"},
+      {stat: "sacks", value: "3"},
       {stat: "fr", value: "2"},
       {stat: "ff", value: "1"}
       ], },
